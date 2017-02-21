a8c440b7edef05ec6f3b69b2203e23053317f095
Wang dong <wangdong@vfinemusic.com>
2017-02-21 15:18:24 +0800

    Test for http response

--- mycelery/app/tasks.py
+++ mycelery/app/tasks.py
@@ -1,6 +1,7 @@
-from celery import task
+from celery import shared_task
+from app.models import Test
 
 
-@task
-def add(x, y):
-    return x + y
+@shared_task
+def create(data):
+    return Test.objects.create(name=data)
--- mycelery/app/views.py
+++ mycelery/app/views.py
@@ -2,25 +2,14 @@
 # Create your views here.
 
 from django.shortcuts import HttpResponse
+from django.http import JsonResponse
 from app import tasks
 
 
 def add_test(request):
 
     # http://www.cnblogs.com/shhnwangjian/category/906800.html
-    result = tasks.add.delay(2, 2)
-    print(result)
-    if result.ready():
-        print("Task has run")
-        if result.successful():
-            print("Result was: %s" % result.result)
-        else:
-            if isinstance(result.result, Exception):
-                print("Task failed due to raising an exception")
-                raise result.result
-            else:
-                print("Task failed without raising exception")
-    else:
-        print("Task has not yet run")
-    return HttpResponse(result)
+    username = request.GET.get('username')
+    tasks.create.delay(username)
+    return JsonResponse({'name': username})
 
